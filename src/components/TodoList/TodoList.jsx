b4145763b4dc8dd62b0a2d8e2beb3da57f3962b7
learnzdevelopmenthub <learnzdevelopmenthub@gmail.com>
2025-01-05 19:09:11 +0530

    added createAsyncThunk

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,33 +1,29 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getTodos } from '../../redux/todoSlice'
+import { getTodosAsync } from '../../redux/todoSlice'
 
 const TodoList = () => {
-	const todos = useSelector(state => state.todos.data)
+	const {data, loading, error} = useSelector(state => state.todos)
   const dispatch = useDispatch()
 
   useEffect(()=>{
-    fetch('https://gorest.co.in/public/v2/users/7616562/todos', {
-			headers: {
-				'Authorization': 'Bearer cf7dc2a04cbe8334b06d1f884660c74333ca7f1739b4fac557d1f0579dc4c932',
-				'Content-Type': 'application/json'
-			}
-		})
-    .then(response=>response.json())
-    .then(data=>{
-      dispatch(getTodos(data))
-    })
+    dispatch(getTodosAsync())
   }, [])
 
+  if (loading) {
+      return <h1>Loading....</h1>
+  }
 	return (
     <>
-      {todos.length === 0 && 
+      {error && <h2>{error}</h2>}
+      
+      {data.length === 0 && 
         <div>
           Todos not found
         </div>
       }
 
-      {todos.length !== 0 && todos.map(item => (
+      {data.length !== 0 && data.map(item => (
         <div key={item.id}>
           <h1>{item.title}</h1>
           <h6>{item.status}</h6>
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -9,20 +9,89 @@
 
 // export default todoReducer;
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const todoSlice = createSlice({
-    name: "todos",
-    initialState: {data: []},
-    reducers: {
-        getTodos: (state, action) => {
-            state.data = action.payload
-        },
-        createTodo: (state, action) => {
-            state.data.unshift(action.payload)
+export const createTodoAsync = createAsyncThunk('todos/createTodo', async(todoData, {rejectWithValue})=>{
+    try{
+        const response = await fetch('https://gorest.co.in/public/v2/users/7616562/todos', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer cf7dc2a04cbe8334b06d1f884660c74333ca7f1739b4fac557d1f0579dc4c932',
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(todoData)
+        })
+
+        if(!response.ok){
+            throw new Error('Error: Todo is not created')
         }
+
+        return await response.json() // for success
+    } catch (error){
+        return rejectWithValue(error.message) // return error text as payload
     }
 })
 
-export const { getTodos, createTodo } = todoSlice.actions;
+export const getTodosAsync = createAsyncThunk('todos/getTodos', async (_, {rejectWithValue})=>{
+    try {
+        const response = await fetch('https://gorest.co.in/public/v2/users/7616562/todos', {
+            headers: {
+				'Authorization': 'Bearer cf7dc2a04cbe8334b06d1f884660c74333ca7f1739b4fac557d1f0579dc4c932',
+				'Content-Type': 'application/json'
+			}
+        })
+
+        if (!response.ok) {
+            throw new Error('Error: Fetching todos is failed')
+        }
+
+        return await response.json()
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const todoSlice = createSlice({
+    name: "todos",
+    initialState: {
+        data: [], 
+        error: null, 
+        loading: false,
+        createError: null,
+        createLoading: false
+    },
+    reducers: {
+        getTodos: (state, action) => {
+            state.data = action.payload
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createTodoAsync.pending, (state)=>{
+                state.createLoading = true
+                state.createError = null
+            })
+            .addCase(createTodoAsync.fulfilled, (state, action)=>{
+                state.createLoading = false
+                state.data.unshift(action.payload)
+            })
+            .addCase(createTodoAsync.rejected, (state, action)=>{
+                state.createLoading = false
+                state.createError = action.payload
+            })
+            .addCase(getTodosAsync.pending, (state)=>{
+                state.loading = true
+                state.error = null
+            })
+            .addCase(getTodosAsync.fulfilled, (state, action)=>{
+                state.loading = false
+                state.data = action.payload
+            }).addCase(getTodosAsync.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
+            })
+    }
+})
+
+export const { getTodos } = todoSlice.actions;
 export default todoSlice.reducer;
